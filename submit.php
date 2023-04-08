afb34f128192e2cfda347d68037d6a9c679155e0
AdrianPortaln01489363 <122579247+AdrianPortal9363@users.noreply.github.com>
2023-04-08 03:47:54 -0400

    first php

--- submit.php
+++ submit.php
@@ -1,25 +1,28 @@
 <?php
-        
-$db_host="localhost";
-$db_username="nftuser";
-$db_passwd="Heavenguard0525.";
-$db_name="nftwebsite";
-$dbc=mysqli_connect($db_host,$db_username,$db_passwd,$db_name) or die ("Could not Connect! \n");
+$db_host = "localhost";
+$db_username = "nftuser";
+$db_passwd = "Heavenguard0525.";
+$db_name = "nftwebsite";
 
-echo "Connection established. \n";
+$dbc = mysqli_connect($db_host, $db_username, $db_passwd, $db_name) or die ("Could not connect! \n");
 
-mysqli_close();
+if ($dbc->connect_error) {
+    die('Connection failed: ' . $dbc->connect_error);
+}
+else {
+    echo "Connection established. \n";
 
-$price = $_POST['price'];
-$name = $_POST['name'];
+    $price = $_POST['price'];
+    $name = $_POST['name'];
 
-$image = $_FILES['file_upload']['name'];
-$temp_name = $_FILES['file_upload']['tmp_name'];
-$path = "uploads/" . $image;
-move_uploaded_file($temp_name, $path);
+    $image = $_FILES['file_upload']['name'];
+    $temp_name = $_FILES['file_upload']['tmp_name'];
+    $path = "uploads/" . $image;
+    move_uploaded_file($temp_name, $path);
 
-$sql = "INSERT INTO nfts (price, name, image) VALUES ('$price', '$name', '$image')";
-$result = mysqli_query($dbc, $sql) or die("Error querying database");
+    $sql = "INSERT INTO nfts (price, name, image) VALUES ('$price', '$name', '$image')";
+    $result = mysqli_query($dbc, $sql) or die("Error querying database");
 
-mysqli_close($dbc);
+    mysqli_close($dbc);
+}
 ?>
